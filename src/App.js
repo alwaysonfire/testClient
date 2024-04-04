a9f8ed4de8211b1ecad82845e14a3f89b142bdd5
euginetan <alwaysonfire2@gmail.com>
2024-04-05 00:56:51 +0800

    Commit for changing get to post

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://18.234.60.224:5000/')
+    axios.post('http://18.234.60.224:5000/')
       .then(response => {
         setMessage(response.data);
         console.log('response.data :>> ', response.data);
